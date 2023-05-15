732804430525af233abb5cbb2bef7f711e8f94f3
xconor <d00261104@student.dkit.ie>
2023-05-15 21:19:32 +0100

    updated homepage text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                    Welcome to ConspiracyBlog
                 </h1>
                 <a 
                     href="/blog"
                     class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
+                    View Blog
                 </a>
             </div>
         </div>
@@ -40,20 +40,20 @@
 
     <div class="text-center p-15 bg-black text-white">
         <h2 class="text-2xl pb-5 text-l"> 
-            I'm an expert in...
+            Here you'll find out everything there is to know about...
         </h2>
 
         <span class="font-extrabold block text-4xl py-1">
-            Ux Design
+            Aliens
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Project Management
+            UFOs
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Digital Strategy
+            Illuminati
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Backend Development
+            Monsters
         </span>
     </div>
 
@@ -63,7 +63,7 @@
         </span>
 
         <h2 class="text-4xl font-bold py-10">
-            Recent Posts
+            Additional Content
         </h2>
 
         <p class="m-auto w-4/5 text-gray-500">
