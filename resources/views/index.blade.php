2a6ba5172a5b149ce6de5c2e48ea3ae17e24ff3c
xconor <d00261104@student.dkit.ie>
2023-05-16 11:27:39 +0100

    updated spotify playlist grid

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -77,6 +77,7 @@
                 <span class="uppercase text-xs">
                     Official Spotify PlayList
                 </span>
+                <br><br/>
                 <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/0HhGjVBHMhQ4OaB5J0mKVl?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
                 <h3 class="text-xl font-bold py-10">
